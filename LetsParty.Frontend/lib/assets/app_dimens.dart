ba786a1c1d358837094b9f04789df53d53a3e6d0
Diana Stoica <diana-c.stoica@outlook.com>
2023-03-20 13:43:46 +0200

    Added new assets

---
+++ LetsParty.Frontend/lib/assets/app_dimens.dart
@@ -0,0 +1,9 @@
+class AppDimens {
+  static const padding = 8.0;
+  static const padding_2x = 16.0;
+  static const padding_3x = 24.0;
+  static const padding_4x = 32.0;
+
+  static const roundedCorners = 16.0;
+  static const roundedButtonCorners = 100.0;
+}

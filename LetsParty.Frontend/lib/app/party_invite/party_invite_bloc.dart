133df38191003d847bec9ef6ac3f579c46fffe70
Diana Stoica <diana-c.stoica@outlook.com>
2023-04-26 21:07:58 +0300

    added parties from backend

--- LetsParty.Frontend/lib/app/party_invite/party_invite_bloc.dart
+++ LetsParty.Frontend/lib/app/party_invite/party_invite_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lets_party_frontend/core/models/invitations_model.dart';
 import 'package:lets_party_frontend/core/models/party_model.dart';
+import 'package:lets_party_frontend/core/repository/invitations_data.dart';
 import 'package:lets_party_frontend/core/repository/party_data.dart';
 
 class PartyInviteBloc extends ChangeNotifier {
@@ -8,12 +10,16 @@ class PartyInviteBloc extends ChangeNotifier {
   }
 
   final PartyData _partyData = PartyData();
+  final InvitationsData _invitationsData = InvitationsData();
   PartyModel? party;
+  List<InvitationsModel> partyGuests = [];
   String? hostName;
   bool loadingDone = false;
 
   Future<void> loadParty(String partyID) async {
     party = await _partyData.getParty(partyID);
+    partyGuests = await _invitationsData.getListOfInvitationsForParty(partyID);
+    print(partyGuests);
     hostName = party!.hostEmail;
     loadingDone = true;
     notifyListeners();

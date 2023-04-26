ae2f77cd91403cce75b3eebb997de2a2b803573a
Diana Stoica <diana-c.stoica@outlook.com>
2023-04-26 21:08:18 +0300

    loaded invitations from backend

---
+++ LetsParty.Frontend/lib/core/repository/invitations_data.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:lets_party_frontend/core/models/invitations_model.dart';
+import 'package:http/http.dart' as http;
+
+class InvitationsData {
+  InvitationsData() {}
+
+  String apiPath = 'http://192.168.1.100:8081/api/v1/invitations';
+
+  Future<List<InvitationsModel>> getListOfInvitationsForParty(String partyId) async {
+    final response = await http.get(Uri.parse('$apiPath/$partyId'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((json) => InvitationsModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load data.');
+    }
+  }
+}

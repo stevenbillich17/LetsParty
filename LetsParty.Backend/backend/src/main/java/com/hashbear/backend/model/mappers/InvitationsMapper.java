bc43ac3001532efc41c448a9bb7b639fc465cdf7
Diana Stoica <diana-c.stoica@outlook.com>
2023-04-27 01:04:59 +0300

    added invitations logic

--- LetsParty.Backend/backend/src/main/java/com/hashbear/backend/model/mappers/InvitationsMapper.java
+++ LetsParty.Backend/backend/src/main/java/com/hashbear/backend/model/mappers/InvitationsMapper.java
@@ -2,6 +2,7 @@ package com.hashbear.backend.model.mappers;
 
 import com.hashbear.backend.model.dtos.InvitationsDTO;
 import com.hashbear.backend.model.entity.Invitations;
+<<<<<<< Updated upstream
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -17,6 +18,37 @@ public class InvitationsMapper {
     public InvitationsDTO invitationsToInvitationsDTO(Invitations invitations) {
         return InvitationsDTO.builder()
                 .partyId(invitations.getPartyId())
+=======
+import com.hashbear.backend.services.PartyService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Mapper(componentModel = "spring")
+public class InvitationsMapper {
+    @Autowired
+    private PartyService partyService;
+
+    public Invitations invitationsDTOtoInvitations(InvitationsDTO invitationsDTO) {
+        final PartyMapper partyMapper = new PartyMapper();
+
+        Invitations invitations = Invitations.builder()
+                .party(partyMapper.partyDTOToParty(partyService.getParty(invitationsDTO.getPartyId())))
+                .invitedEmail(invitationsDTO.getInvitedEmail())
+                .status(invitationsDTO.getStatus())
+                .build();
+
+        return invitations;
+    }
+
+    public InvitationsDTO invitationsToInvitationsDTO(Invitations invitations) {
+        final PartyMapper partyMapper = new PartyMapper();
+        return InvitationsDTO.builder()
+                .partyId(partyMapper.partyToPartyDTO(invitations.getParty()).getId())
+>>>>>>> Stashed changes
                 .invitedEmail(invitations.getInvitedEmail())
                 .status(invitations.getStatus())
                 .build();

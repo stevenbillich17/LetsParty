accd538d98e2bc9ae4c4a52bc41c86f1d747a5f7
Diana Stoica <diana-c.stoica@outlook.com>
2023-04-27 13:03:22 +0300

    added header in api request

--- LetsParty.Frontend/lib/core/repository/invitations_data.dart
+++ LetsParty.Frontend/lib/core/repository/invitations_data.dart
@@ -7,8 +7,12 @@ class InvitationsData {
 
   String apiPath = 'http://192.168.1.100:8081/api/v1/invitations';
 
+  Map<String, String> headers = {
+    "Authorization": "Bearer token"
+  };
+
   Future<List<InvitationsModel>> getListOfInvitationsForParty(String partyId) async {
-    final response = await http.get(Uri.parse('$apiPath/party/$partyId'));
+    final response = await http.get(Uri.parse('$apiPath/party/$partyId'), headers: headers);
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => InvitationsModel.fromJson(json)).toList();
@@ -18,7 +22,7 @@ class InvitationsData {
   }
 
   Future<List<InvitationsModel>> getListOfInvitationsForUser(String mail) async {
-    final response = await http.get(Uri.parse('$apiPath/email/$mail'));
+    final response = await http.get(Uri.parse('$apiPath/email/$mail'), headers: headers);
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => InvitationsModel.fromJson(json)).toList();
--- LetsParty.Frontend/lib/core/repository/party_data.dart
+++ LetsParty.Frontend/lib/core/repository/party_data.dart
@@ -5,6 +5,11 @@ import 'package:http/http.dart' as http;
 class PartyData {
   PartyData() {}
 
+  Map<String, String> headers = {
+    "Authorization": "Bearer token"
+  };
+
+
   String apiPath = 'http://192.168.1.100:8081/api/v1/parties';
   List<PartyModel> hostedParties = [];
 
@@ -14,22 +19,23 @@ class PartyData {
   }
 
   Future<List<PartyModel>> getListOfGoingParties() async {
-    final response = await http.get(Uri.parse(apiPath));
+    final response = await http.get(Uri.parse(apiPath), headers: headers);
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => PartyModel.fromJson(json)).toList();
     } else {
+      print(response.statusCode);
       throw Exception('Failed to load data.');
     }
   }
 
   Future<PartyModel> getParty(String id) async {
-    final response = await http.get(Uri.parse('$apiPath/$id'));
+    final response = await http.get(Uri.parse('$apiPath/$id'), headers: headers);
     if (response.statusCode == 200) {
       Map<String, dynamic> data = json.decode(response.body);
       return PartyModel.fromJson(data);
     } else {
-      throw Exception('Failed to load data.');
+      throw Exception('Failed to load data!!!');
     }
   }
 }
